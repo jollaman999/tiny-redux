0b969abdbe954b8c9dd8a819c2e99adcaff10e96
jollaman999 <admin@jollaman999.com>
2022-01-09 21:44:32 +0900

    Create a setter to execute the state change function passed by the user

--- redux.js
+++ redux.js
@@ -1,8 +1,13 @@
-export function createStore(INITIAL_STATE) {
+export function createStore(INITIAL_STATE, updater) {
   let state;
 
   if (!state) {
     state = INITIAL_STATE;
+  }
+
+  function doUpdater(data) {
+    // state 변경을 앱이 원하는 시점에서 실행할 수 있도록 반환하는 state 변경 함수이다.
+    state = updater(state, data);
   }
 
   function getState() {
@@ -11,5 +16,6 @@ export function createStore(INITIAL_STATE) {
 
   return {
     getState, // state를 바로 반환할 경우 값을 직접 참조하게 되므로 getter를 반환한다.
+    doUpdater,
   }
 }
